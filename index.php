5cb90a3dfbca3d824f40a10299a834c86e82b646
Eligio Biasi <ex_98@live.it>
2023-07-14 19:36:24 +0200

    set up card structure

--- index.php
+++ index.php
@@ -12,8 +12,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet Shop</title>
+    <link rel="stylesheet" href=".\style.css">
 </head>
     <body>
-        
+        <div class="container">
+                <?php foreach ($negozio as $item) { ?>
+                    <div class="card">
+                        <div>
+                            <img src="<?php $item->getImage()?>" alt="">
+                        </div>
+
+                        <ul>
+                            <li>
+                                <?php echo $item->getName()?>
+                            </li>
+
+                            <li>
+                            <?php echo $item->getPrice()?>
+                                
+                            </li>
+
+                            <li>
+                            <?php echo $item->getDescription()?>
+                                
+                            </li>
+
+                            <li>
+                            <?php echo $item->getBrand()?>
+                                
+                            </li>
+
+                            <li>
+                            <?php echo $item->getAnimal()?>
+                                
+                            </li>
+                        </ul>
+                        
+                    </div>
+                <?php }?>
+        </div>
     </body>
 </html>
